f6d5fc30c2565bc685a46168cc2739f42fa17d5c
Kim L <plantsntech@protonmail.com>
2021-05-08 02:09:42 -0400

    Updated site

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
+setInterval(showTime, 1000);
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    let currentTime = hour + ":" 
+        + min + ":" + sec + am_pm;
+
+    document.getElementById("clock")
+        .innerHTML = currentTime;
+}
+
+showTime();
 
 
+var months = [
+"January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December",
+];
+function myTimer() {
+var date = new Date();
+document.getElementById("date").innerHTML =
+date.getDate() +
+"-" +
+months[date.getMonth()] +
+"-" +
+date.getFullYear();
+}
 
-document.querySelector('#tip-form').onchange = function(){
-
-    var bill = Number(document.getElementById('billTotal').value);
-    var tip = document.getElementById('tipInput').value;
-    document.getElementById('tipOutput').innerHTML = `${tip}%`;
-    var tipValue = bill * (tip/100)
-    var finalBill = bill + tipValue
-  console.log(finalBill)
-  var tipAmount = document.querySelector('#tipAmount')
-  var totalBillWithTip = document.querySelector('#totalBillWithTip')
-  
-  tipAmount.value = tipValue.toFixed(2);
-   totalBillWithTip.value =finalBill.toFixed(2);
-  
-   //Show Results
-  
-    document.getElementById('bTn').style.display='block'
-  }
-  
+myTimer()
